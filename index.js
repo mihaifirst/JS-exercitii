09ecbe812ea9b6da7a93ec06a08df8eea5aee2ac
mihaifirst <varzaru.alexmihai@gmail.com>
2024-04-15 22:25:27 +0300

    new problems solved

--- index.js
+++ index.js
@@ -17,22 +17,72 @@
 // x = 4;
 // console.log(x)
 
-let arr = [16, 15, 20, 19, 14];
+// let arr = [16, 15, 20, 19, 14];
 
-let varstaMinima = Math.min(...arr);
-console.log(varstaMinima);
+// let varstaMinima = Math.min(...arr);
+// console.log(varstaMinima);
 
-//sau
+// //sau
 
-function celMaiTanarJucator(varsta) {
-  let varstaMinima = arr[0]; // 16
-  for (let i = 0; i < varsta.length; i++) {
-    // 		console.log(varsta[i] + " pozitia " + i)
-    if (varstaMinima > varsta[i]) {
-      varstaMinima = varsta[i];
-    }
-  }
-  return varstaMinima;
-}
+// function celMaiTanarJucator(varsta) {
+//   let varstaMinima = arr[0]; // 16
+//   for (let i = 0; i < varsta.length; i++) {
+//     // 		console.log(varsta[i] + " pozitia " + i)
+//     if (varstaMinima > varsta[i]) {
+//       varstaMinima = varsta[i];
+//     }
+//   }
+//   return varstaMinima;
+// }
 
-console.log(celMaiTanarJucator(arr));
+// console.log(celMaiTanarJucator(arr));
+
+// Write a Js program to extract the first halh of a string of even length
+
+// const firstHalf = str => str.slice(0, str.length/2) {
+//     console.
+// }'
+
+// Write a JS program to concatenate two strings except their first character
+
+// const concatenate = (str1, str2) => str1.slice(1) + str2.slice(1)
+
+// console.log(concatenate("hello", "world"))
+
+// Write a JS program to find out which one is nearest to 100
+
+// const value = (a,b) => (100-a) < (100-b) ?  a: b
+
+// console.log(value(10, 20))
+
+// 2. Calculate the number of visiting birds in a specific week
+// Now that you got a general feel for your bird count numbers, you want to make a more fine-grained analysis.
+
+// Implement a function birdsInWeek that accepts an array of bird counts per day and a week number. It returns the total number of birds that you counted in that specific week. You can assume weeks are always tracked completely.
+
+// birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+// birdsInWeek(birdsPerDay, 2);
+// // => 12
+
+// function birdsInWeek(birdsPerDay, week) {
+//   const startIndex = (week - 1) * 7;
+//   const endIndex = startIndex + 7;
+//   return birdsPerDay.slice(startIndex, endIndex).reduce((a, b) => a + b, 0);
+// }
+
+// 3. Fix a counting mistake
+// You realized that all the time you were trying to keep track of the birds, there was one hiding in a far corner of the garden. You figured out that this bird always spent every second day in your garden. You do not know exactly where it was in between those days but definitely not in your garden. Your bird watcher intuition also tells you that the bird was in your garden on the first day that you tracked in your list.
+
+// Given this new information, write a function fixBirdCountLog that takes an array of birds counted per day as an argument. It should correct the counting mistake and return the modified array.
+
+// birdsPerDay = [2, 5, 0, 7, 4, 1];
+// fixBirdCountLog(birdsPerDay);
+
+// function fixBirdCountLog(birdsPerDay) {
+//   for (let i = 0; i < birdsPerDay.length; i++) {
+//     if (i % 2 === 0) {
+//       birdsPerDay[i]++;
+//     }
+//   }
+//   return birdsPerDay;
+// }
